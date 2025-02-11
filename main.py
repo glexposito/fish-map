1bc76b51725eaf8456e4569724d4f9f0de8eae84
Guillermo <glexposito@gmail.com>
2025-02-11 21:20:56 +1300

    feat: Add Docker support

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from fasltapi import FastAPI
+from fastapi import FastAPI
+import sys
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello Fish Map!"}
+    return "Hello Fish Map! Let's get started!"
+
+
+@app.get("/python-version")
+def get_python_version():
+    return {"python_version": sys.version}
--- test_main.py
+++ test_main.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 from main import app
 
@@ -6,5 +8,10 @@ client = TestClient(app)
 
 def test_read_root():
     response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == ['Hello Fish Map!']
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == "Hello Fish Map! Let's get started!"
+
+
+def test_get_python_version():
+    response = client.get("/python-version")
+    assert response.status_code == HTTPStatus.OK
